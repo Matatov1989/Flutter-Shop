7c3b797203d4cfddd5b17fac7a775b2725e73fd9
Yury <matatov1989@gmail.com>
2023-03-07 21:53:48 +0200

    update status and send order

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import './cart.dart';
-
 
 class OrderItem {
   final String id;
@@ -8,12 +9,11 @@ class OrderItem {
   final List<CartItem> products;
   final DateTime dateTime;
 
-  OrderItem({
-    required this.id,
-    required this.amount,
-    required this.products,
-    required this.dateTime
-  });
+  OrderItem(
+      {required this.id,
+      required this.amount,
+      required this.products,
+      required this.dateTime});
 }
 
 class Orders with ChangeNotifier {
@@ -23,13 +23,29 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(0, OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        products: cartProducts,
-        dateTime: DateTime.now()
-    ));
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://tnopis-default-rtdb.europe-west1.firebasedatabase.app/orders.json';
+    final timestamp = DateTime.now();
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts.map((cp) => {
+                'id': cp.id,
+                'title': cp.title,
+                'quantity': cp.quantity,
+                'price': cp.price
+              })
+        })
+    );
+
+    _orders.insert(
+        0,
+        OrderItem(
+            id: json.decode(response.body)['name'],
+            amount: total,
+            products: cartProducts,
+            dateTime: timestamp));
     notifyListeners();
   }
 }
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -35,16 +35,7 @@ class CartScreen extends StatelessWidget {
                       style: TextStyle(color: Theme.of(context).primaryTextTheme.titleLarge?.color),),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  TextButton(
-                    child: Text('ORDER NOW', style: TextStyle(color: Theme.of(context).primaryColor,)),
-                    onPressed: () {
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                          cart.items.values.toList(),
-                          cart.totalAmount,
-                      );
-                      cart.clear();
-                    },
-                  ),
+                  OrderButton(cart: cart),
                 ],
               ),
             ),
@@ -64,6 +55,42 @@ class CartScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key? key,
+    required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: _isLoading ? CircularProgressIndicator() : Text('ORDER NOW', style: TextStyle(color: Theme.of(context).primaryColor,)),
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading) ? null : () async {
+        setState(() {
+          _isLoading = true;
+        });
+        await Provider.of<Orders>(context, listen: false).addOrder(
+            widget.cart.items.values.toList(),
+            widget.cart.totalAmount,
+        );
+        setState(() {
+          _isLoading = false;
+        });
+        widget.cart.clear();
+      },
     );
   }
 }
